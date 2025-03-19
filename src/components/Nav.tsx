7b6370461225aa228a74a6ef5fa3118ad483b30c
bebzbzbz <balzerbeatrice@gmail.com>
2025-03-19 12:03:29 +0100

    nav, splash screen + intro

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,27 @@
+import { Link, useLocation } from "react-router-dom";
+
 const Nav = () => {
+    const location = useLocation();
+    const homeActive = location.pathname === "/home"
+
     return ( 
-        <></>
-     );
+        <nav className="flex justify-between items-center px-12 py-8 fixed bottom-0 left-0
+         right-0">
+            <Link to="/home" className={`flex gap-3 text-sm items-center ${homeActive && "text-[var(--main-red)]"}`}>
+                <img src="/images/homeIcon.svg" alt="Home Icon" />
+                Home
+            </Link>
+            <Link to="/home">
+                <img src="/images/saveIcon.svg" alt="Save Icon" />
+            </Link>
+            <Link to="/home">
+                <img src="/images/storageIcon.svg" alt="Storage Icon" />
+            </Link>
+            <Link to="/home">
+                <img src="/images/profileIcon.svg" alt="Profile Icon" />
+            </Link>
+        </nav>
+    );
 }
- 
+
 export default Nav;
--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -1,7 +1,16 @@
+import SplashScreen from "../components/SplashScreen";
+
 const Intro = () => {
     return ( 
-        <></>
-     );
+        <>
+            {/* <SplashScreen/> */}
+            <img src="/images/Intro_Pic.png" alt="" className="h-100 w-100 object-cover"/>
+            <article className="text-center py-10 px-7">
+                <h2 className="text-3xl/11 mb-2">Enjoy Your Movie, <br/> Watch Everywhere</h2>
+                <p>Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.</p>
+            </article>
+        </>
+    );
 }
- 
+
 export default Intro;
