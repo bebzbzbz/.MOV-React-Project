9e0e07f4ea91de061c6015d80557e35410f22f3c
YvonneJL <yvonneloder@gmx.de>
2025-03-19 11:10:25 +0100

    home

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
 import Layout from "./layout/Layout"
+import Intro from "./pages/Intro"
+import Home from "./pages/Home"
 
 
 function App() {
 
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<Layout/>}>
-
+      <Route index element={<Intro/>}/>
+      <Route path="/home" element={<Home/>}/>
     </Route>
   ))
 
