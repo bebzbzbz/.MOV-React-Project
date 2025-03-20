9b60459c78b1f621f01e625276a9f2cb424d2fad
YvonneJL <yvonneloder@gmx.de>
2025-03-20 16:35:34 +0100

    changed z-Index

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -1,6 +1,6 @@
 const SplashScreen = () => {
     return (  
-        <div className="splashscreen h-screen flex justify-center items-center bg-main-red intro-slide fixed top-[-100%] z-10 w-full">
+        <div className="splashscreen h-screen flex justify-center items-center bg-main-red intro-slide fixed top-[-100%] z-30 w-full">
             <p className="text-white font-bold text-5xl scaled-text">.MOV</p>
         </div>
     );
